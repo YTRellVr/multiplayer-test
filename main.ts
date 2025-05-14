3efd719e3aa5510fa3a62e768273c7af9b8f4984
YTRellVr <R4llalt@gmail.com>
2025-05-14 13:09:09 -0400

    Update pxt.json, main.ts, images.g.jres, images.g.ts

--- main.ts
+++ main.ts
@@ -1,16 +1,23 @@
 //set up
-tiles.setCurrentTilemap(tilemap`level`)
-scene.setBackgroundImage(assets.image`Background`)
+forever(function() {
+    mp.allPlayers()
+
+})
+scene.setBackgroundImage(assets.image`background`)
 //Players
 let Player1 = sprites.create(assets.image`Player1`, SpriteKind.Player)
-Player1.setScale(2, ScaleAnchor.Middle)
+Player1.setScale(1.5, ScaleAnchor.Middle)
 Player1.setStayInScreen(false)
-controller.player1.moveSprite(Player1)
 let Player2 = sprites.create(assets.image`Player2`, SpriteKind.Player)
-Player2.setScale(2, ScaleAnchor.Middle)
+Player2.setScale(1.5, ScaleAnchor.Middle)
 Player2.setStayInScreen(false)
-controller.player2.moveSprite(Player2)
 
-
-
+//Screen stuff
 scene.cameraFollowSprite(Player1)
+tiles.setCurrentTilemap(tilemap`level`)
+
+//Movement p1
+controller.player1.moveSprite(Player1)
+
+//Movement p2
+controller.player2.moveSprite(Player2)
